33e1a4ecfdd623545cb7d5a6d939c360289e3e34
Reece Pohlman <reecepohlman@gmail.com>
2019-10-23 10:45:49 -0400

    operator function is now working

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-/*
-okay so where the fuck do i even begin ?
-i shoudl create the basic functions like addition, multiplication and so on
-need to add button functions
-start with the basic addition and subtraction.
-*/
+let method;
+let numbers = {
+  reactantOne: 0,
+  reactantTwo: 0,
+  operater: 0,
+};
 
 function add(a, b) {
   return a + b;
@@ -17,12 +17,39 @@ function multiplication(a, b) {
 function divide(a, b) {
   return a / b;
 }
-function power(a, b) {
-  return (a ** b);
+function operate(a, operater, b) {
+  console.log(a);
+  console.log(b);
+  console.log(operater);
+    if (operater == '+') {
+      console.log('test');
+      return add(a, b);
+    }else if (operater == '-') {
+      return minus(a ,b);
+    }else if (operater == 'x') {
+      return multiplication(a, b);
+    }else if (operater == '÷') {
+      return divide(a, b);
+  };
 }
-function operate(operater, a, b) {
-  return operater(a, b);
+function display(number) {
+  return function(){
+    para.textContent = number
+    reactantOne = parseInt(number);
+  }
 }
+function store() {
+
+}
+/*
+so the display function needs to take a number entered and display but also store it as a varible to be used in the operate
+function, but how can i get the three arguments that operater needs when display can only store one and once its click and will
+overide the last saved varible.
+
+instead of calling display when clicked i should call a new function that will store the number entered in a varible in as an
+array, or object; then once you click enter it will use the array to call varibles for the operate function
+
+*/
 
 const nine = document.querySelector('#nine');
 const eight = document.querySelector('#eight');
@@ -38,3 +65,27 @@ const allClear = document.querySelector('#allclear');
 const dot = document.querySelector('#dot');
 const pow = document.querySelector('#power');
 const result = document.querySelector('#result');
+const para = document.createElement('p');
+const plus = document.querySelector('#add');
+const sub = document.querySelector('#sub');
+const times = document.querySelector('#mult');
+const div = document.querySelector('#div');
+const equal = document.querySelector('#equal');
+result.appendChild(para)
+
+one.addEventListener('click', display('1'));
+two.addEventListener('click', display('2'));
+three.addEventListener('click', display('3'));
+four.addEventListener('click', display('4'));
+five.addEventListener('click', display('5'));
+six.addEventListener('click', display('6'));
+seven.addEventListener('click', display('7'));
+eight.addEventListener('click', display('8'));
+nine.addEventListener('click', display('9'));
+zero.addEventListener('click', display('0'));
+dot.addEventListener('click', display('.'));
+plus.addEventListener('click', display('+'));
+sub.addEventListener('click', display('-'));
+div.addEventListener('click', display('÷'));
+times.addEventListener('click', display('x'));
+//equal.addEventListener('click', operate());
