3fa197b6d4f7833763e9b0a672fc0a1803d117f4
Reece Pohlman <reecepohlman@gmail.com>
2019-10-24 21:44:32 -0400

    I believe i have figured out how to make, added how in comment

---
+++ script.js
@@ -0,0 +1,77 @@
+/*
+okay how the fuck am i going to do this
+*/
+let number1 = [];
+let number2 = [];
+let swap = false;
+
+function plus(a, b) {
+  return a + b;
+}
+function minus(a, b) {
+  return a - b;
+}
+
+function operate(a, operater, b) {
+  if (operater == '+') {
+    return plus(a, b);
+  }else if (operater == '-') {
+    return minus(a, b);
+  }
+}
+function store(){
+
+}
+
+function display(number) {
+  para.textContent = number;
+}
+
+
+const nine = document.querySelector('#nine');
+const eight = document.querySelector('#eight');
+const seven = document.querySelector('#seven');
+const six = document.querySelector('#six');
+const five = document.querySelector('#five');
+const four = document.querySelector('#four');
+const three = document.querySelector('#three');
+const two = document.querySelector('#two');
+const one = document.querySelector('#one');
+const clear = document.querySelector('#clear');
+const allClear = document.querySelector('#allclear');
+const dot = document.querySelector('#dot');
+const pow = document.querySelector('#power');
+const result = document.querySelector('#result');
+const para = document.createElement('p');
+const plus = document.querySelector('#add');
+const sub = document.querySelector('#sub');
+const times = document.querySelector('#mult');
+const div = document.querySelector('#div');
+const equal = document.querySelector('#equal');
+result.appendChild(para)
+
+//buttons
+one.addEventListener('click', () => {return store('1');})
+two.addEventListener('click', () => {return store('2');});
+three.addEventListener('click', () => {return store('3');});
+four.addEventListener('click', () => {return store('4');});
+five.addEventListener('click', () => {return store('5');});
+six.addEventListener('click', () => {return store('6');});
+seven.addEventListener('click', () => {return store('7');});
+eight.addEventListener('click', () => {return store('8');});
+nine.addEventListener('click', () => {return store('9');});
+zero.addEventListener('click', () => {return store('0');});
+dot.addEventListener('click', () => {return store('.');});
+plus.addEventListener('click', () => {return store('+');});
+sub.addEventListener('click', () => {return store('-');});
+div.addEventListener('click', () => {return store('÷');});
+times.addEventListener('click', () => {return store('x');});
+
+// okay so how do i get numbers into the number1 array and the other numbers into number2 after the operater button is pressed?
+// have input numbers go into ann array.
+// MOTHER FUCKING EUREKA
+// i need to have two arrays and an varible set to false, and once you press any of the operater buttons
+// it will turn the varible to true.
+// while the varible is false all the input numbers will be added to number1 and while true they will be added to number2.
+// turn arrays into strings, then strings into intergers.
+// then when you press the equal button it will call the operate function.
