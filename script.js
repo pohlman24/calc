e08762cec554fd16b36351510f025c52c11830cd
Reece Pohlman <reecepohlman@gmail.com>
2019-10-27 20:39:15 -0400

    made progress on operating previous answer

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ okay how the fuck am i going to do this;
 */
 let number1 = [];
 let number2 = [];
+let answer = [];
 let int1;
 let int2;
 let swap = false;
 let op;
 let test;
+let ran;
+let i = 0;
 
 function plus(a, b) {
   return a + b;
@@ -15,7 +18,7 @@ function plus(a, b) {
 function minus(a, b) {
   return a - b;
 }
-function  times(a, b) {
+function times(a, b) {
   return a * b;
 }
 function divided(a, b) {
@@ -24,16 +27,20 @@ function divided(a, b) {
 
 function operate(a, operater, b) {
   if (operater == '+') {
-    let answer = plus(a, b);
+    answer[i] = plus(a, b);
+    i++;
     return(display(answer))
   }else if (operater == '-') {
-    let answer = minus(a, b);
+    answer[i] = minus(a, b);
+    i ++;
     return(display(answer))
   }else if (operater == 'x') {
-    let answer = times(a, b);
+    answer[i] = times(a, b);
+    i++;
     return(display(answer))
   }else if (operater == '÷') {
-    let answer = divided(a, b);
+    answer[i] = divided(a, b);
+    i++;
     return(display(answer))
   }
 }
@@ -43,18 +50,24 @@ function store(number){
     number1.push(number);
     int1 = number1.join('');
     int1 = parseFloat(int1);
+    if(answer.length >= 1){
+      int1 = answer[i];
+    }
     return (display(int1));
   }else if(swap == true && number != '+' && number != '-' && number != 'x' && number != '÷') {
+    if(answer.length >= 1){
+      number2 = [];
+    }
     number2.push(number);
     int2 = number2.join('');
     int2 = parseFloat(int2);
     return (display(test + int2));
-  }else if (number == '+' || number == '-' || number == 'x' || number == '÷' && swap == false){
+  }else if (number == '+' || number == '-' || number == 'x' || number == '÷' || number == '=' && swap == false){
     swap = true;
     op = number;
     test = int1 + ' ' + op + ' ';
     return (display(test));
-  }else if (number == '+' || number == '-' || number == 'x' || number == '÷' && swap == true){
+  }else if (number == '+' || number == '-' || number == 'x' || number == '÷' || number == '=' && swap == true){
     swap = false;
     op = number;
     test = int1 + ' ' + op + ' ';
@@ -82,7 +95,7 @@ const three = document.querySelector('#three');
 const two = document.querySelector('#two');
 const one = document.querySelector('#one');
 const clear = document.querySelector('#clear');
-const allClear = document.querySelector('#allclear');
+const neg = document.querySelector('#neg');
 const dot = document.querySelector('#dot');
 const pow = document.querySelector('#power');
 const result = document.querySelector('#result');
@@ -122,3 +135,9 @@ clear.addEventListener('click', () => {return Clear();});
 // while the varible is false all the input numbers will be added to number1 and while true they will be added to number2.
 // turn arrays into strings, then strings into intergers.
 // then when you press the equal button it will call the operate function.
+
+
+//okay so know i need it to be set so that when you get an answer that answer will be stored in a array and
+//when you want to do another operator to it, it will take the array[i] and then plug that into the operater function
+//then that new answer will be stored in as array[1] and repeat.
+//each time you get a new answer i goes up.
